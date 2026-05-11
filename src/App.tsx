c47348b9db2dc8fdcf9bdec9636ef32404a8b252
Martin Fischer <mamfischer92@gmail.com>
2026-05-11 21:37:24 +0200

    chore: add basic empty project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section>
+        <div>
+          <h1>Elevsøk</h1>
+        </div>
+      </section>      
+    </>
+  )
+}
+
+export default App
