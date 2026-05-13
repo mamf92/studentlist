dd7079e1827af48e3112b79299e90bffe29c6523
Martin Fischer <mamfischer92@gmail.com>
2026-05-13 13:45:51 +0200

    feat: add search and sort of students

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
+import StudentSearch from './pages/StudentSearch'
+
 function App() {
   return (
     <>
       <section>
         <div className="bg-background">
-          <h1 className="text-heading text-5xl font-inter">Elevsøk</h1>
-          <p className="text-mutedbackground text-lg font-sometype-mono">
-            Søk etter elever i din klasse
-          </p>
+          <StudentSearch />
         </div>
       </section>
     </>
